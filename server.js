aa80bf70f240c446ab5f81980e3c8cdb9b6460c3
OJPARKINSON <olliepar01@gmail.com>
2021-11-26 20:54:39 +0000

    added amplify

--- server.js
+++ server.js
@@ -1,103 +1,80 @@
-const express = require('express')
-const session = require('express-session');
-const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify').Strategy
-const axios =  require('axios');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const refresh = require('passport-oauth2-refresh');
+import { Strategy } from "passport-spotify";
+import session from "express-session";
+import passport from "passport";
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import next from "next";
 
-require('dotenv').config();
+import {
+  ensureAuthenticated,
+  ensureNotExpired,
+  getUnixDate,
+} from "./units/index.js";
 
-var port = 8888;
-var authCallbackPath = '/callback';
+dotenv.config();
 
-const app = express();
+var port = process.env.PORT || 3001;
+var authCallbackPath = "/callback";
 
-app.use(express.static('public'))
-    .use(cookieParser())
-    .use(morgan('tiny'))
-    .use(session({ secure: true, secret: 'keyboard cat', saveUninitialized: false }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use(cors());
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const host = process.env.host || "localhost";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-    
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
+app.prepare().then(() => {
+  const server = express();
 
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
-    
-const strategy = new SpotifyStrategy(
-    {
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:' + port + authCallbackPath,
+  const sessionConfig = {
+    secret: uid.sync(18),
+    cookie: {
+      maxAge: 86400 * 1000, // 24 hours in milliseconds
     },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-        process.nextTick(function() {
-            return done(null, {profile, accessToken} );
-        });
+    resave: false,
+    saveUninitialized: true,
+  };
+  server.use(session(sessionConfig));
+
+  const spotifyStrategy = new Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: host + port + authCallbackPath,
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      process.nextTick(function () {
+        const expiryTime = expires_in * 1000 + getUnixDate();
+
+        return done(null, { profile, accessToken, refreshToken, expiryTime });
+      });
     }
-);
-        
-refresh.use(strategy);
-passport.use(strategy);
+  );
 
-app.get('/login', 
-    passport.authenticate('spotify', {
-        scope: ['user-read-private user-read-playback-state streaming user-read-email user-read-private user-read-currently-playing']
-    }),
-    
-);
+  passport.use(spotifyStrategy);
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
 
-app.get('/callback',
-    passport.authenticate('spotify', { failureRedirect: 'http://localhost:3000/' }),
-    function(req, res) {
-        res.redirect('http://localhost:3000/album');
-    }
-);
+  // 5 - adding Passport and authentication routes
+  server.use(passport.initialize());
+  server.use(passport.session());
 
-app.get('/logout', function(req, res) {
-    req.session.albumCache = null;
-    req.logout();
-    res.redirect('http://localhost:3000/');
+  passport.serializeUser(function (user, done) {
+    done(null, user);
   });
 
-app.get('/album', ensureAuthenticated, async (req, res) => {
-    console.log()
-    return axios({
-        url: "https://api.spotify.com/v1/me/player/currently-playing",
-        method: "get",
-        headers: {
-            'Authorization': 'Bearer ' + req.user.accessToken,
-            Accept: "application/json"
-        }
-    })
-        .then(async response => {
-            if (response.status === 204) {
-                res.send({error: "Please make sure music is playing"});
-            } else if (req.session.albumCache === response.data.item.album.id) {
-                res.sendStatus(204);
-            } else {
-                const img = response.data.item.album.images[0].url
-                req.session.dateUpdated = response.headers.date
-                req.session.albumCache = response.data.item.album.id;
-                res.send({url: img, name: response.data.item.album.name});
-            }
-        })
-        .catch(error => console.log(error)) 
+  passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+  });
+
+  server.use("/auth", authRouter);
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('http://localhost:3000/');
-  }
-
-app.listen(8888);
