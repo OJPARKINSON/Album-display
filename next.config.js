3e649d2c707a644286970bc5d6ae0550b5d9bd7f
OJPARKINSON <olliepar01@gmail.com>
2021-12-21 03:44:04 +0000

    cleaned up next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,4 @@ module.exports = {
   images: {
     domains: ['i.scdn.co'],
   },
-  env: {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    BASE_URL: process.env.BASE_URL,
-    SCOPE: process.env.SCOPE,
-  },
 };
