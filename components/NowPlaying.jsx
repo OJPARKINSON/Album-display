545de5191418b66f2361b606d5cb19d28b8d303c
OJPARKINSON <olliepar01@gmail.com>
2022-01-14 23:53:39 +0000

    added a blurred m

--- components/NowPlaying.jsx
+++ components/NowPlaying.jsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import fetcher from "lib/fetcher";
 
 export default function NowPlaying() {
-  const ops = { refreshInterval: 4500 }; // 4 seconds
+  const ops = { refreshInterval: 4000 }; // 4 seconds
   const { data, error } = useSWR("/api/now-playing", fetcher, ops);
   const { albumImageUrl, artist, title, isPodcast, isPlaying } = data || {};
 
@@ -12,19 +12,30 @@ export default function NowPlaying() {
   if (isPodcast) return <p>Podcast playing</p>;
   if (!isPlaying) return <p>Nowt Playing</p>;
 
+  const style = {
+    backgroundImage: `url(${albumImageUrl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <div>
-      <Image
-        src={albumImageUrl}
-        alt={title}
-        width={600}
-        height={600}
-        layout="fixed"
-        className="block shadow-outline self-center"
-      />
-      <p>
-        {title} by {artist}
-      </p>
+    <div style={style}>
+      <div className="backdrop-blur-xl m-0 p-0 w-screen h-screen flex items-center justify-center text-center ">
+        <div>
+          <Image
+            src={albumImageUrl}
+            alt={title}
+            width={600}
+            height={600}
+            layout="fixed"
+            className="block shadow-outline self-center"
+          />
+          <p>
+            {title} by {artist}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import NowPlaying from "components/NowPlaying";
-import {getTweets} from "../lib/twitter";
+import { getTweets } from "../lib/twitter";
 
-export default ({tweets}) => (
-    <div
-        className="m-0 p-0 w-screen h-screen flex items-center justify-center text-center bg-gradient-to-r from-violet-500 to-fuchsia-500">
-        <NowPlaying/>
-        {tweets.map(({media, url}) => <img key={media} src={url}/>)}
-    </div>
-)
 
+export default ({ tweets }) => {
+ return (
+  <div>
+    <NowPlaying />
+    {tweets.map(({ media, url }) => (
+      <img key={media} src={url} />
+    ))}
+  </div>
+)};
 
 export async function getStaticProps() {
-    const tweets = await getTweets();
+  const tweets = await getTweets();
 
-    return {props: {tweets}};
+  return { props: { tweets } };
 }
