3660489edf9af99147bc077efe5385710a81fa13
Oliver Parkinson <olliepar01@gmail.com>
2022-02-16 22:53:15 +0000

    environment varibles work around

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   images: {
     domains: ["i.scdn.co", "pbs.twimg.com"],
   },
+  env: {
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+  },
   reactStrictMode: true,
 };
