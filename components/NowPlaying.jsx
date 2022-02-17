4a0983ee88c5217430071f68f3d5edf07c92230a
Oliver Parkinson <olliepar01@gmail.com>
2022-02-17 00:29:20 +0000

    Forgot how slow the default interval is

--- components/NowPlaying.jsx
+++ components/NowPlaying.jsx
@@ -4,12 +4,13 @@ import { useLastFM } from "use-last-fm";
 const clientID = process.env.LASTFM_CLIENT_ID;
 
 export default function NowPlaying() {
-  const { status, song } = useLastFM("OJPARKINSON", clientID);
+  const { status, song } = useLastFM("OJPARKINSON", clientID, 4000);
 
-  if (status === "error") return <p>error</p>;
-  if (!status) return <p>loading...</p>;
+  console.log(song);
 
   if (status === "idle") return <p>Nowt Playing</p>;
+  if (status === "error") return <p>error</p>;
+  if (song?.art == null) return <p>loading...</p>;
 
   const bonkArt = song?.art.replace("300x300", "1500x1500");
 
