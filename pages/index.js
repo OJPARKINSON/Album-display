d380782c0aa1132e7ba945301cb1efbb6114d90c
OJPARKINSON <olliepar01@gmail.com>
2022-03-28 00:45:13 +0100

    spent too much time looking into twitter api

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import { getTweets } from "lib/twitter";
 import useSWR from "swr";
-import fetcher from "lib/fetcher";
+import { fetcher } from "lib/spotify";
+import { getTweets } from "lib/twitter";
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 
 export default function index({ tweets }) {
+  let refreshInterval = 4500;
   const { data, error } = useSWR(NOW_PLAYING_ENDPOINT, fetcher, {
-    refreshInterval: 5000,
+    refreshInterval,
     refreshWhenHidden: true,
   });
 
-  if (error !== undefined) return <p>error</p>;
-  if (data === null) {
+  if (error !== undefined || data === null) {
+    refreshInterval = 10000;
     return tweets.map(({ media, url }) => <img key={media} src={url} />);
   }
 
