c19adf56f7cdc9e5ba77f6e450abd8327081fe20
Oliver Parkinson <olliepar01@gmail.com>
2023-12-30 17:42:44 +0000

    removed the twitter part

--- api/bonk.go
+++ api/bonk.go
@@ -2,23 +2,46 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/pusher/pusher-http-go/v5"
 )
 
-type Profile struct {
-	Name		string
-	Hobbies []string
+
+type Payload struct {
+    Url string `json:"url" `
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+	if (r.Method == "POST") {
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		var data Payload 
+		json.Unmarshal(reqBody, &data)
 
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		pusherClient := pusher.Client{
+			AppID: "1577349",
+			Key: "1971fe9418e861720cec",
+			Secret: "492ad61909e86fef685c",
+			Cluster: "eu",
+			Secure: true,
+		}
+
+		fmt.Println(data, "golang")
+		
+		err := pusherClient.Trigger("my-channel", "my-event", data)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			return;
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		return;
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(409)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module bonk
 
 go 1.17
+
+require (
+	github.com/pusher/pusher-http-go/v5 v5.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+)
